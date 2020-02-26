cb61a786dfbf581393f0a8eec5848230cb08acf5
norio c <norionorioc@gmail.com>
2020-02-26 05:46:02 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,33 @@
 class Waiter
-
+  
+  attr_accessor :name, :experience
+  
+  @@all = [] 
+  
+  def initialize(name, experience)
+      @name = name 
+      @experience = experience
+      @@all << self 
+  end 
+  
+  def self.all 
+      @@all 
+  end
+  
+  def new_meal(customer, total, tip=0)
+      Meal.new(self, customer, total, tip)
+  end
+  
+  def meals 
+      Meal.all.select do 
+        |meal| meal.waiter == self
+      end
+  end
+  
+  def best_tipper 
+      best_tipped = meals.max do |meal1, meal2|
+        meal1.tip <=> meal2.tip
+  end
+  best_tipped.customer
+end
 end
